90a63b9e03d476c721a899a1fad6a46847ff697b
Stefano Romanò <rumix87@gmail.com>
2021-07-10 17:43:31 +0200

    Added Javadoc for Layer interface

--- src/main/java/com/github/steromano87/pig4j/layers/Layer.java
+++ src/main/java/com/github/steromano87/pig4j/layers/Layer.java
@@ -2,6 +2,18 @@ package com.github.steromano87.pig4j.layers;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Generic transformation layer that is applied to an image
+ *
+ * An image is composed as the superimposition of multiple layers, each of them adding an effect to the final image
+ * (e.g. adding a blurring effect, some text blocks and so on).
+ */
 public interface Layer {
+    /**
+     * Applies the current layer to the image outputted by the previous layer
+     *
+     * @param image the previously outputted image
+     * @return the image after being processed by the layer
+     */
     BufferedImage apply(BufferedImage image);
 }
