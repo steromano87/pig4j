a60740b13a2b672bac7c5b887be95a228f281682
Stefano Romanò <rumix87@gmail.com>
2021-07-12 20:10:07 +0200

    Updated tests to simulate the image generation with a custom image position

--- src/test/java/com/github/steromano87/pig4j/test/ImageGeneratorTests.java
+++ src/test/java/com/github/steromano87/pig4j/test/ImageGeneratorTests.java
@@ -1,10 +1,13 @@
 package com.github.steromano87.pig4j.test;
 
 import com.github.steromano87.pig4j.ImageGenerator;
+import com.github.steromano87.pig4j.exceptions.ImageGenerationException;
+import com.github.steromano87.pig4j.layers.base.SingleColorLayer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 class ImageGeneratorTests {
     @Test
@@ -23,5 +26,31 @@ class ImageGeneratorTests {
     void testSimpleInstantiationWithAlphaChannel() {
         ImageGenerator generator = new ImageGenerator(640, 480, null,true);
         Assertions.assertInstanceOf(ImageGenerator.class, generator);
+    }
+
+    @Test
+    void testNoLayersException() {
+        ImageGenerator generator = new ImageGenerator(640, 480);
+        Assertions.assertThrows(
+                ImageGenerationException.class,
+                generator::build
+        );
+    }
+
+    @Test
+    void testOutputAsJavaImage() {
+        ImageGenerator generator = new ImageGenerator(640, 480);
+        SingleColorLayer layer = new SingleColorLayer();
+        layer.setColor(Color.BLUE);
+        generator.addLayer(layer);
+
+        BufferedImage outputImage = generator.build().toImage();
+
+        Assertions.assertAll(
+                () -> Assertions.assertInstanceOf(BufferedImage.class, outputImage),
+                () -> Assertions.assertEquals(640, outputImage.getWidth()),
+                () -> Assertions.assertEquals(480, outputImage.getHeight()),
+                () -> Assertions.assertFalse(outputImage.getColorModel().hasAlpha())
+        );
     }
 }
--- src/test/java/com/github/steromano87/pig4j/test/layers/base/ImageLayerTests.java
+++ src/test/java/com/github/steromano87/pig4j/test/layers/base/ImageLayerTests.java
@@ -2,6 +2,7 @@ package com.github.steromano87.pig4j.test.layers.base;
 
 import com.github.steromano87.pig4j.ImageGenerator;
 import com.github.steromano87.pig4j.layers.base.ImageLayer;
+import com.github.steromano87.pig4j.options.PositionOptions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -48,6 +49,38 @@ class ImageLayerTests {
         ImageLayer secondImageLayer = new ImageLayer();
         Path secondImagePath = Paths.get("src/test/resources/common", "trollface.png");
         secondImageLayer.setImageFile(secondImagePath.toFile());
+        generator.addLayer(secondImageLayer);
+
+        BufferedImage generatedImage = generator.build().toImage();
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(640, generatedImage.getWidth(), "Generated image width mismatch"),
+                () -> Assertions.assertEquals(400, generatedImage.getHeight(), "Generated image height mismatch"),
+                () -> Assertions.assertFalse(generatedImage.getColorModel().hasAlpha(), "Final image should have alpha channel")
+        );
+    }
+
+    @Test
+    void testCenteredImagePositionGeneration() {
+        ImageGenerator generator = new ImageGenerator(640, 400);
+
+        // Add first image
+        ImageLayer imageLayer = new ImageLayer();
+        Path imagePath = Paths.get("src/test/resources/common", "landscape_640_400.jpg");
+        imageLayer.setImageFile(imagePath.toFile());
+        generator.addLayer(imageLayer);
+
+        // Add second image
+        ImageLayer secondImageLayer = new ImageLayer();
+        Path secondImagePath = Paths.get("src/test/resources/common", "trollface.png");
+        secondImageLayer.setImageFile(secondImagePath.toFile());
+
+        PositionOptions positionOptions = new PositionOptions();
+        positionOptions.setImageHook(PositionOptions.Hook.MID_CENTER);
+        positionOptions.setX(320);
+        positionOptions.setY(200);
+        secondImageLayer.setPositionOptions(positionOptions);
+
         generator.addLayer(secondImageLayer);
 
         BufferedImage generatedImage = generator.build().toImage();
